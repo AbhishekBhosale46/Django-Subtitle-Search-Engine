2ab06a43bd13bc8abd8f53d13e46d79602253e93
Abhishek BHosale <abhisheksachinbhosale@gmail.com>
2023-12-17 20:08:11 +0530

    feat: added basic video file uploading functionality

--- search_engine/views.py
+++ search_engine/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UploadVideoForm
 
 def home(request):
     return render(request, 'search_engine/home.html')
 
 def upload_video(request):
-    return render(request, 'search_engine/upload_video.html')
+
+    if request.method == 'POST':
+
+        form = UploadVideoForm(request.POST, request.FILES)
+
+        if form.is_valid():
+
+            """ Extract neccessary details """
+            video_file = request.FILES['file']
+            video_file_name = str(video_file).split('.')[0]
+            video_file_extension = str(video_file).split('.')[1]
+
+            """ Store the video temporarily in media directory """
+            temp_video_file_path = 'media/temp.'+video_file_extension
+            with open(temp_video_file_path, 'wb') as temp_video_file:
+                for chunk in video_file.chunks():
+                    temp_video_file.write(chunk)
+
+            """ Enqueue the task into task queue (redis) """
+
+
+            return redirect('home')
+    else:
+        form = UploadVideoForm()
+
+    return render(request, 'search_engine/upload_video.html', {'form': form})
 
 def search_video(request):
     return render(request, 'search_engine/search_video.html')
